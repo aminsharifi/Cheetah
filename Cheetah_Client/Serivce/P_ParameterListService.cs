f9ab64545e41724968a54c3359f611bb2cd1cb83
aminsharifi <m.amin.sharifi@gmail.com>
2023-02-07 17:37:55 +0330

    Upload WASM

--- Cheetah_Client/Serivce/P_ParameterListService.cs
+++ Cheetah_Client/Serivce/P_ParameterListService.cs
@@ -1,32 +1,32 @@
 ﻿using Newtonsoft.Json;
-using Cheetah_Client.Service.IService;
 using Cheetah_Models;
 using Cheetah_Models.Parameters;
+using Cheetah_Business.Repository.IRepository;
 
 namespace Cheetah_Client.Service
 {
-    
-    public class P_ParameterListService : IP_ParameterListService
+    public class P_ParameterListService : IP_ParameterListRepository
     {
+
         private readonly HttpClient _httpClient;
         private IConfiguration _configuration;
         private string BaseServerUrl;
         public P_ParameterListService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
-            _configuration=configuration;
+            _configuration = configuration;
             BaseServerUrl = _configuration.GetSection("BaseServerUrl").Value;
         }
 
-        public async Task<P_ParameterListDTO> Get(Int64 Id)
+        public async Task<P_ParameterListDTO> Get(long RecordId)
         {
-            var response = await _httpClient.GetAsync($"/P_ParameterList/{Id}");
+            var response = await _httpClient.GetAsync($"/P_ParameterList/{RecordId}");
             var content = await response.Content.ReadAsStringAsync();
             if (response.IsSuccessStatusCode)
             {
-                var P_ParameterList = JsonConvert.DeserializeObject<P_ParameterListDTO>(content);
-                //product.ImageUrl = BaseServerUrl+product.ImageUrl;
-                return P_ParameterList;
+                var p_ParameterList = JsonConvert.DeserializeObject<P_ParameterListDTO>(content);
+                //product.ImageUrl=BaseServerUrl+product.ImageUrl;
+                return p_ParameterList;
             }
             else
             {
@@ -41,14 +41,29 @@ namespace Cheetah_Client.Service
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
-                var p_ParameterList = JsonConvert.DeserializeObject<IEnumerable<P_ParameterListDTO>>(content);
-                foreach(var prod in p_ParameterList)
+                var ParameterLists = JsonConvert.DeserializeObject<IEnumerable<P_ParameterListDTO>>(content);
+                foreach (var prod in ParameterLists)
                 {
                     //prod.ImageUrl=BaseServerUrl+prod.ImageUrl;
                 }
-                return p_ParameterList;
+                return ParameterLists;
             }
             return new List<P_ParameterListDTO>();
+        }
+
+        public Task<P_ParameterListDTO> Update(P_ParameterListDTO obj_DTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<P_ParameterListDTO> Create(P_ParameterListDTO obj_DTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> delete(long id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
