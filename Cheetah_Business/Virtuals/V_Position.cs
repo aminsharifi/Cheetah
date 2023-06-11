bbdf93aff22544651a5f296b9e44f70b65a5f14a
aminsharifi <m.amin.sharifi@gmail.com>
2023-06-11 19:46:00 +0430

    update virtual tables

--- Cheetah_Business/Virtuals/V_Position.cs
+++ Cheetah_Business/Virtuals/V_Position.cs
@@ -1,18 +1,21 @@
-﻿using Cheetah_Business.Data;
-using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace Cheetah_Business.Virtuals
 {
     [Table(nameof(V_Position), Schema = nameof(TableType.Virtuals))]
-    [Index(nameof(PCode), IsUnique = true, AllDescending = true)]
-    [Index(nameof(PIndex), IsUnique = true, AllDescending = true)]
-    [Index(nameof(PName), IsUnique = true, AllDescending = true)]
-    [Index(nameof(CreateTimeRecord), IsUnique = true, AllDescending = true)]
-    [Index(nameof(LastUpdatedRecord), IsUnique = true, AllDescending = true)]
-    [Index(nameof(PERPCode), IsUnique = false, AllDescending = true)]
-    [Index(nameof(DsblRecord), IsUnique = false, AllDescending = true)]
-    [Index(nameof(Parent_Id), IsUnique = false, AllDescending = true)]
-    public partial class V_Position : BaseClass<V_Position>
+    
+    public partial class V_Position 
     {
+        [Column(Order = 101)]
+        [Key]
+        public Int64? PERPCode { get; set; }
+
+        [Column(Order = 102)]
+        [StringLength(256)]
+        public String? PName { get; set; }
+
+        [Column(Order = 103)]
+        [StringLength(256)]
+        public String? PDisplayName { get; set; }
     }
 }
--- Cheetah_Business/Virtuals/V_User.cs
+++ Cheetah_Business/Virtuals/V_User.cs
@@ -1,22 +1,9 @@
-﻿using Cheetah_Business.Data;
-using Cheetah_Business.Facts;
-using Cheetah_Business.Links;
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace Cheetah_Business.Virtuals
 {
     [Table(nameof(V_User), Schema = nameof(TableType.Virtuals))]
-    [Index(nameof(PCode), IsUnique = true, AllDescending = true)]
-    [Index(nameof(PIndex), IsUnique = true, AllDescending = true)]
-    [Index(nameof(PName), IsUnique = true, AllDescending = true)]
-    [Index(nameof(CreateTimeRecord), IsUnique = true, AllDescending = true)]
-    [Index(nameof(LastUpdatedRecord), IsUnique = true, AllDescending = true)]
-    [Index(nameof(PERPCode), IsUnique = false, AllDescending = true)]
-    [Index(nameof(Parent_Id), IsUnique = false, AllDescending = true)]
-    [Index(nameof(DsblRecord), IsUnique = false, AllDescending = true)]
-    public partial class V_User : BaseClass<V_User>
+    public partial class V_User
     {
         public V_User()
         {
@@ -27,39 +14,36 @@ namespace Cheetah_Business.Virtuals
         #endregion
 
         #region Simple Prob
-        [StringLength(25)]
+
+        [Column(Order = 101)]
+        [Key]
+        public Int64? PERPCode { get; set; }
+
+        [StringLength(128)]
         [Column(Order = 102)]
-        public string? User_Domain { get; set; }
+        public String? PName { get; set; }
+
+        [StringLength(128)]
         [Column(Order = 103)]
-        public bool? User_EnabledForAssignation { get; set; }
+        public String? PDisplayName { get; set; }
+
         [Column(Order = 104)]
-        public bool? User_DelegateEnabled { get; set; }
+        public Int64? User_BossUserId  { get; set; }
+
         [Column(Order = 105)]
-        public bool? User_CreatedCasesSkipAssigRules { get; set; }
-        [StringLength(10)]
-        [Column(Order = 106)]
-        public string? User_IDPersonel { get; set; }
-        [StringLength(20)]
-        [Column(Order = 107)]
-        public string? User_LDAPDescription { get; set; }
+        public Boolean? DsblRecord { get; set; }
 
         #endregion
 
         #region Relations
 
-        #region Entity   
-
-        #region S_User
-        [Column(Order = 108)]
-        public long? User_BossUserId { get; set; }
-
-        [Column(Order = 109)]
-        public long? User_DelegateId { get; set; }
-
-        #endregion              
+        #region Entity
 
         #endregion
-      
+
+        #region Collection
+
+        #endregion
 
         #endregion
     }
