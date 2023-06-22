6d5affd864bf6a730035e93536ec0ea69788255c
aminsharifi <m.amin.sharifi@gmail.com>
2023-06-22 17:26:57 +0430

    JWT

---
+++ Cheetah_WebAPI/Helper/APISettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Cheetah_WebAPI.Helper
+{
+    public class APISettings
+    {
+        public string SecretKey { get; set; }
+        public string ValidAudience { get; set; }
+        public string ValidIssuer { get; set; }
+    }
+}
