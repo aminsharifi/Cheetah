7822bcfb3f0c3ec8ce1aae49e518cefec8e93014
aminsharifi <m.amin.sharifi@gmail.com>
2023-06-24 14:21:32 +0330

    update JWT

--- Cheetah_WebAPI/Program.cs
+++ Cheetah_WebAPI/Program.cs
@@ -1,7 +1,14 @@
 using Cheetah_Business.Repository;
+using Cheetah_DataAccess;
 using Cheetah_DataAccess.Data;
 using Cheetah_DataAccess.Repository;
+using Cheetah_WebAPI.Helper;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,17 +44,55 @@ else
 }
 
 
+/**/
+
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+var apiSettingsSection = builder.Configuration.GetSection("APISettings");
+builder.Services.Configure<APISettings>(apiSettingsSection);
+ 
+var apiSettings = apiSettingsSection.Get<APISettings>();
+var key = Encoding.ASCII.GetBytes(apiSettings.SecretKey);
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidAudience = apiSettings.ValidAudience,
+        ValidIssuer = apiSettings.ValidIssuer,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddScoped(typeof(IIdentityService), typeof(IdentityService));
+
+/**/
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped(typeof(ISimpleClassRepository), typeof(SimpleClassRepository));
-
 
 builder.Services.AddGraphQLServer()
     .AddMutationType<Mutation>()
     .AddQueryType<Query>()
     .AddProjections()
     .AddFiltering()
-    .AddSorting();
+    .AddAuthorization()
+    .AddSorting()
+    .BindRuntimeType<char, StringType>();
 
 //https://www.c-sharpcorner.com/article/graphql-introduction-and-product-application-using-net-core-7/
 
@@ -61,15 +106,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.   
 //if (app.Environment.IsDevelopment())
 //{
-app.UseSwagger();
-app.UseSwaggerUI();
+//app.UseSwagger();
+//app.UseSwaggerUI();
 //}
 
 app.UseHttpsRedirection();
 
 app.UseCors("Cheetah");
 
-//app.UseRouting();
+app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
