002ffef80615c670884874d4a67403a879b9bb35
aminsharifi <m.amin.sharifi@gmail.com>
2023-06-24 16:36:51 +0330

    update postman

--- Cheetah_Business/Data/CartableDTO.cs
+++ Cheetah_Business/Data/CartableDTO.cs
@@ -2,29 +2,31 @@
 
 public class CartableDTO
 {
-    CPersianClass cPersianClass = new CPersianClass();
-    public String Username { get; set; }
-    public String Requestor { get; set; }
+    private CPersianClass? cPersianClass = new CPersianClass();
+    public String? Username { get; set; }
+    public String? Requestor { get; set; }
     public String? RadNumber { get; set; }
     public DateTime? CreateDate { get; set; }
-    public String? PCreateDate
+    public  String? PCreateDate
     {
-        get { return cPersianClass.GetPersianDate(this.CreateDate); }
+        get {return cPersianClass.GetPersianDate(this.CreateDate); }
+        set { PCreateDate = value; }
     }
     public DateTime? RecieveDate { get; set; }
     public String? PRecieveDate
     {
         get { return cPersianClass.GetPersianDate(this.RecieveDate); }
+        set { PRecieveDate = value; }
     }
     public String? ProcessName { get; set; }
     public String? TaskName { get; set; }
     public string? Summary { get; set; }
-    public CartableDTO()
-    {
+    //public CartableDTO()
+    //{
 
-    }
-    public CartableDTO(String username)
-    {
-        Username = username;
-    }
+    //}
+    //public CartableDTO(String username)
+    //{
+    //    Username = username;
+    //}
 }
