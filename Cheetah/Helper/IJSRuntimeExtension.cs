66be45dcb198c3bcd2976ce398864f358b9c7c4a
aminsharifi <m.amin.sharifi@gmail.com>
2023-07-28 15:54:05 +0430

    eliminate toastr,popper an sweetalert

--- Cheetah/Helper/IJSRuntimeExtension.cs
+++ Cheetah/Helper/IJSRuntimeExtension.cs
@@ -4,14 +4,14 @@ namespace Cheetah.Helper
 {
     public static class IJSRuntimeExtension
     {
-        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
-        {
-            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
-        }
-
-        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
-        {
-            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
-        }
+        //public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        //{
+        //    await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        //}
+        
+        //public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        //{
+        //    await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        //}
     }
 }
