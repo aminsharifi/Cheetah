b4ef5a1d3373ad5ba6f08be9c3df2295ede9f653
aminsharifi <m.amin.sharifi@gmail.com>
2023-08-17 18:17:20 +0330

    Finalyzed Edit

--- Cheetah_Business/Repository/IWorkItem.cs
+++ Cheetah_Business/Repository/IWorkItem.cs
@@ -6,7 +6,7 @@ namespace Cheetah_Business.Repository
     {
         public Task SetCartable(F_WorkItem f_WorkItem, F_Condition ExpectedCondition);
         public Task SetCurrentAssignment(F_WorkItem f_WorkItem);
-        public Task SetWorkItemsAsync(F_Case Current_Case);
+        public Task SetWorkItemsAsync(F_Case Current_Case, F_WorkItem? Current_WorkItem = null);
         public Task<F_Case> CreateRequestAsync(F_Case request);
         public Task<F_WorkItem> PerformWorkItemAsync(F_WorkItem f_WorkItem);
         public Boolean CompareCondition(IEnumerable<F_Condition> Actual_Conditions,
