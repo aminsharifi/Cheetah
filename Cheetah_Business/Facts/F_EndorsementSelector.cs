b548adba5dc1238bc9eb6b3b46bbd1935f0a419d
aminsharifi <m.amin.sharifi@gmail.com>
2023-09-03 13:35:56 +0330

    Implement UI for multi Condionditions and multi Endorsments

---
+++ Cheetah_Business/Facts/F_EndorsementSelector.cs
@@ -0,0 +1,39 @@
+﻿using Cheetah_Business.Data;
+using Cheetah_Business.Dimentions;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Cheetah_Business.Facts;
+
+[Table(nameof(F_EndorsementSelector), Schema = nameof(TableType.Facts))]
+[Index(nameof(Name), IsUnique = true, AllDescending = true)]
+[Index(nameof(CreateTimeRecord), IsUnique = true, AllDescending = true)]
+[Index(nameof(LastUpdatedRecord), IsUnique = true, AllDescending = true)]
+[Index(nameof(ERPCode), IsUnique = false, AllDescending = true)]
+[Index(nameof(EnableRecord), IsUnique = false, AllDescending = true)]
+//[Index(nameof(Parent_Id), IsUnique = false, AllDescending = true)]
+public partial class F_EndorsementSelector : SimpleClass
+{
+    [Column(Order = 100)]
+    public long? EndorsementId { get; set; }
+    public virtual F_Endorsement? Endorsement { get; set; }
+
+    [Column(Order = 101)]
+    public long? EndorsementItemId { get; set; }
+    [ForeignKey(nameof(EndorsementItemId))]
+    public virtual F_EndorsementItem? EndorsementItem { get; set; }
+
+    public override void SetName()
+    {
+        DisplayName = Endorsement?.DisplayName + "," + SortIndex;
+        Name = Endorsement?.Name + "," + SortIndex ;
+    }
+
+    #region Functions
+    public F_EndorsementSelector ShallowCopy()
+    {
+        return (F_EndorsementSelector)this.MemberwiseClone();
+    }  
+
+    #endregion
+
+}
