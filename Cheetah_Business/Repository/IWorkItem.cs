b37bcc444b80055140668822eeaa3253aa314732
aminsharifi <m.amin.sharifi@gmail.com>
2023-09-10 11:17:13 +0330

    OutputState

--- Cheetah_Business/Repository/IWorkItem.cs
+++ Cheetah_Business/Repository/IWorkItem.cs
@@ -8,7 +8,7 @@ namespace Cheetah_Business.Repository
         public Task SetCurrentAssignment(F_WorkItem f_WorkItem);
         public Task SetWorkItemsAsync(F_Case Current_Case, F_WorkItem? Current_WorkItem = null);
         public Task<Tuple<F_Case, SimpleClassDTO>> CreateRequestAsync(F_Case request);
-        public Task<F_WorkItem> PerformWorkItemAsync(F_WorkItem f_WorkItem);
+        public Task<Tuple<F_WorkItem, SimpleClassDTO>> PerformWorkItemAsync(F_WorkItem f_WorkItem);
         public Boolean CompareCondition(IEnumerable<F_Condition> Actual_Conditions,
             IEnumerable<F_Condition> Expected_Conditions);
     }
