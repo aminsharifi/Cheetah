8656ae5ad6bdf79ea8eb1723acbce50bd23cdef9
aminsharifi <m.amin.sharifi@gmail.com>
2023-09-28 14:46:16 +0330

    rename projects

---
+++ Cheetah.Presentation.Web.Blazor.Server/Data/WeatherForecast.cs
@@ -0,0 +1,16 @@
+namespace Cheetah.Presentation.Web.Blazor.Server.Data;
+
+public class WeatherForecast
+{
+    public String? RadNumber { get; set; }
+
+    public DateTime? CreateDate { get; set; }
+
+    public String? Creator { get; set; }
+
+    public String? ProcessName { get; set; }
+
+    public String? TaskName { get; set; }
+
+    public string? Summary { get; set; }
+}
---
+++ Cheetah.Presentation.Web.Blazor.Server/Program.cs
@@ -0,0 +1,125 @@
+using Cheetah.Application.Business.Repository;
+using Cheetah.Domain;
+using Cheetah.Infrastructure.Persistence;
+using Cheetah.Infrastructure.Persistence.Repository;
+using Cheetah.Presentation.Web.Blazor.Server.Data;
+using Cheetah.Presentation.Web.Blazor.Server.Services;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+#region Serilog
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration)); 
+#endregion
+
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+builder.Services.AddTransient<DbInitialiser>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBootstrapBlazor();
+
+//var DomainName = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+
+var provider = builder.Configuration.GetValue("Provider", "Npgsql");
+
+if (provider is "Npgsql")
+{
+    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+    builder.Services.AddDbContext<ApplicationDbContext>(
+        b => b.UseLazyLoadingProxies()
+        .UseNpgsql(builder.Configuration.GetConnectionString("Npgsql")
+        , x => x.MigrationsAssembly("cheetah_data_access_npgsql")
+        ),
+        ServiceLifetime.Transient
+        );
+}
+else
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(
+        b => b.UseLazyLoadingProxies()
+        .UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"),
+        x => x.MigrationsAssembly("cheetah_data_access_sqlserver")),
+        ServiceLifetime.Transient
+        );
+}
+
+
+//builder.Services.AddDefaultIdentity<ApplicationUser>()
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddDefaultTokenProviders().AddDefaultUI()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped(typeof(ITableCRUD), typeof(TableCRUD));
+builder.Services.AddScoped(typeof(ITableCRUD), typeof(TableCRUD));
+builder.Services.AddScoped(typeof(IWorkItem), typeof(WorkItem));
+builder.Services.AddScoped(typeof(IView), typeof(View));
+builder.Services.AddScoped(typeof(ISync), typeof(Sync));
+builder.Services.AddScoped(typeof(ICartable), typeof(Cartable));
+builder.Services.AddScoped(typeof(ICopyClass), typeof(CopyClass));
+
+
+
+builder.Services.AddScoped<CNavigation>();
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+var initialiser = services.GetRequiredService<DbInitialiser>();
+
+await initialiser.Run();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+SeedDatabase();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapBlazorHub();
+
+app.MapFallbackToPage("/_Host");
+
+app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
