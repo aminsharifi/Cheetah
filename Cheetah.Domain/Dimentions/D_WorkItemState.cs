e1e1438d09c14c37b72d57ba8f94754b73e89fc1
aminsharifi <m.amin.sharifi@gmail.com>
2023-11-11 11:43:50 +0330

    change anotation to fluent for dimentions

--- Cheetah.Domain/Dimentions/D_WorkItemState.cs
+++ Cheetah.Domain/Dimentions/D_WorkItemState.cs
@@ -1,6 +1,4 @@
 ﻿namespace Cheetah.Domain.Dimentions;
-
-[Table(nameof(D_WorkItemState), Schema = nameof(TableType.Dimentions))]
 public partial class D_WorkItemState : SimpleClass
 {
     #region Relations
