3a0f9ae801c173235b81025ffc8b4d981221767b
aminsharifi <m.amin.sharifi@gmail.com>
2023-11-13 12:52:46 +0330

    add editorconfig to project

--- src/Cheetah.Domain/GlobalUsings.cs
+++ src/Cheetah.Domain/GlobalUsings.cs
@@ -2,11 +2,12 @@
 global using Cheetah.Domain.Dimentions;
 global using Cheetah.Domain.Facts;
 global using Cheetah.Domain.Links;
+global using Cheetah.Domain.Resx;
+global using Cheetah.Resx;
 global using FluentResults;
 global using Serilog.Events;
 global using Serilog.Formatting;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using System.Globalization;
 global using System.Net;
-global using System.Reflection;
 global using System.Resources;
