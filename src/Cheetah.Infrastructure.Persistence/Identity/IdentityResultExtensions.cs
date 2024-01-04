3bb91ebdc9ef9db05bd0c6a320c214ae30aad759
aminsharifi <m.amin.sharifi@gmail.com>
2024-01-04 22:39:20 +0330

    Add DDD to the Persistence layer

---
+++ src/Cheetah.Infrastructure.Persistence/Identity/IdentityResultExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Cheetah.Infrastructure.Persistence.Identity;
+
+public static class IdentityResultExtensions
+{
+    public static Result ToApplicationResult(this IdentityResult result)
+    {
+        return result.Succeeded
+            ? Result.Success()
+            : Result.Failure(result.Errors.Select(e => e.Description));
+    }
+}
