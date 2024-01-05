a26e68da5e31fdb9aebd0a3079f73922ce24ece2
aminsharifi <m.amin.sharifi@gmail.com>
2024-01-05 10:26:15 +0330

    update IdentityUser

--- src/Cheetah.Infrastructure.Persistence.Providers.SqlServer/Migrations/20240105065420_add_ApplicationUser.Designer.cs
+++ src/Cheetah.Infrastructure.Persistence.Providers.SqlServer/Migrations/20240105065420_add_ApplicationUser.Designer.cs
@@ -4,6 +4,7 @@ using Cheetah.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cheetah.Infrastructure.Persistence.Providers.SqlServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240105065420_add_ApplicationUser")]
+    partial class add_ApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
