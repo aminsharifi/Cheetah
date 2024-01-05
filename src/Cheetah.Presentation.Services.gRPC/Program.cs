c44ea600bb90a9a7691968c5baf3536e2bd73f34
aminsharifi <m.amin.sharifi@gmail.com>
2024-01-05 21:55:11 +0330

    Add WebApplicationBuilder to the Persistence Layer

--- src/Cheetah.Presentation.Services.gRPC/Program.cs
+++ src/Cheetah.Presentation.Services.gRPC/Program.cs
@@ -6,61 +6,8 @@ if (builder.Environment.IsProduction())
     {
         serverOptions.ListenAnyIP(1989, cfg => { cfg.Protocols = HttpProtocols.Http2; });
         serverOptions.ListenAnyIP(1990, cfg => { cfg.Protocols = HttpProtocols.Http1; });
-    });
-    builder.Host.ConfigureAppConfiguration((_, config) => { config.Sources.Clear(); });
-    builder.Configuration.AddConsul(Environment.GetEnvironmentVariable("Key_Consul") ?? string.Empty,
-        options =>
-        {
-            options.ConsulConfigurationOptions =
-                cco =>
-                {
-                    cco.Address =
-                        new Uri(Environment.GetEnvironmentVariable("Address_Consul") ?? string.Empty);
-                    cco.Token = Environment.GetEnvironmentVariable("Token_Consul");
-                };
-            options.Optional = true;
-            options.PollWaitTime = TimeSpan.FromSeconds(5);
-            options.ReloadOnChange = true;
-        });
+    });    
 }
-
-#region Serilog
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration));
-#endregion
-
-
-var provider = builder.Configuration.GetValue("Provider", "Npgsql");
-
-if (provider is "Npgsql")
-{
-    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-    builder.Services.AddDbContext<ApplicationDbContext>(
-        b => b.UseLazyLoadingProxies()
-        .UseNpgsql(builder.Configuration.GetConnectionString("Npgsql")
-        , x => x.MigrationsAssembly("Cheetah.Infrastructure.Persistence.Providers.Npgsql")
-        ),
-        ServiceLifetime.Transient
-        );
-}
-else
-{
-    builder.Services.AddDbContext<ApplicationDbContext>(
-        b => b.UseLazyLoadingProxies()
-        .UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"),
-        x => x.MigrationsAssembly("Cheetah.Infrastructure.Persistence.Providers.SqlServer")),
-        ServiceLifetime.Transient
-        );
-}
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddScoped(typeof(ITableCRUD), typeof(TableCRUD));
-builder.Services.AddScoped(typeof(IWorkItem), typeof(WorkItem));
-builder.Services.AddScoped(typeof(ICartable), typeof(Cartable));
-builder.Services.AddScoped(typeof(ICopyClass), typeof(CopyClass));
-
-//builder.Services.AddGrpc();
 
 builder.Services.AddGrpc(options =>
 {
@@ -70,15 +17,13 @@ builder.Services.AddGrpc(options =>
     }
 });
 
-
-
 builder.Services.AddTransient<ExceptionMiddleware>();
-var app = builder.Build();
+
+var app = await builder.InitialiseDatabaseAsync();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<RequestService>();
-app.UseSerilogRequestLogging();
 app.UseMiddleware<ExceptionMiddleware>();
 app.MapGet("/health", () => Results.Ok());
 app.Run();
