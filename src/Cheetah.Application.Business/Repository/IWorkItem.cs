0c61f5aa247bc7184d0681e12ff9a927b7a567bc
AminSharifi <m.amin.sharifi@gmail.com>
2024-02-19 17:47:09 +0330

    change create case for support conditional state

--- src/Cheetah.Application.Business/Repository/IWorkItem.cs
+++ src/Cheetah.Application.Business/Repository/IWorkItem.cs
@@ -5,7 +5,7 @@ public interface IWorkItem
     public Task<CheetahResult<Boolean>> SetWorkItemsAsync(F_Case Current_Case, F_WorkItem? Current_WorkItem = null);
     public Task<CheetahResult<F_Case>> CreateRequestAsync(F_Case request);
     public Task<CheetahResult<F_WorkItem>> PerformWorkItemAsync(F_WorkItem f_WorkItem, Boolean Rebase = false);
-    public CheetahResult<Boolean> CompareCondition(IEnumerable<F_Condition> Actual_Conditions, IEnumerable<F_Condition> Expected_Conditions);
+    public Boolean CompareCondition(IEnumerable<F_Condition> Actual_Conditions, IEnumerable<F_Condition> Expected_Conditions);
     public Task<CheetahResult<F_WorkItem>> SetCurrentAssignment(F_WorkItem Current_WorkItem);
     public Task<CheetahResult<L_CaseTaskUser>> SetCaseTaskUser(L_CaseTaskUser CaseTaskUser);
 }
