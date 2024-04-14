08a41914a05d6b06dad13f05d456abd9d4d471e1
AminSharifi <m.amin.sharifi@gmail.com>
2024-04-14 15:01:10 +0330

    feat: The sync service has been cleaned code and moved to the application layer

---
+++ src/Cheetah.Application.Business/Entity/Specifications/GetAllERPCodesSpec.cs
@@ -0,0 +1,10 @@
+﻿namespace Cheetah.Application.Business.Entity.Specifications;
+public class GetAllERPCodesSpec<T> : Specification<T, long?> where T : BaseEntity
+{
+    public GetAllERPCodesSpec()
+    {
+        Query.AsNoTracking();
+
+        Query.Select(x => x.ERPCode);
+    }
+}
