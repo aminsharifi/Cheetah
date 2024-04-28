9465470731b106e24b3e7420ff07554ed120dba1
AminSharifi <m.amin.sharifi@gmail.com>
2024-04-28 19:26:49 +0330

    fix: Inbox/Outbox services

--- src/Cheetah.Application.Business/Case/WorkItem/Get/GetCartableHandler.cs
+++ src/Cheetah.Application.Business/Case/WorkItem/Get/GetCartableHandler.cs
@@ -6,21 +6,50 @@ public class GetCartableHandler(
     IReadRepository<F_Condition> _conditionRepository,
     IReadRepository<D_User> _userRepository,
     IReadRepository<F_Case> _caseRepository,
+    IReadRepository<D_CaseState> _caseStateRepository,
     IReadRepository<F_Task> _taskRepository,
     ISender _ISender, ICopyClass _iCopyClass)
   : IQueryHandler<GetCartableQuery, Result<IEnumerable<CartableDTO>>>
 {
     public async Task<Result<IEnumerable<CartableDTO>>> Handle(GetCartableQuery request, CancellationToken cancellationToken)
     {
+        if (request.cartableDTO.User is not null)
+        {
+            GetIdEntitySpec<D_User> _getIdEntitySpec = new(request.cartableDTO.User);
+
+            var _userId = await _userRepository.FirstOrDefaultAsync(_getIdEntitySpec);
+
+            request.cartableDTO.User = new() { Id = _userId.Value };
+        }
+        if (request.cartableDTO.Process is not null)
+        {
+            GetIdEntitySpec<D_Process> _getIdEntitySpec = new(request.cartableDTO.Process);
+
+            var _processId = await _processRepository.FirstOrDefaultAsync(_getIdEntitySpec);
+
+            request.cartableDTO.Process = new() { Id = _processId.Value };
+        }
+        if (request.cartableDTO.CaseState is not null)
+        {
+            GetIdEntitySpec<D_CaseState> _getIdEntitySpec = new(request.cartableDTO.CaseState);
+
+            var _caseState = await _caseStateRepository.FirstOrDefaultAsync(_getIdEntitySpec);
+
+            request.cartableDTO.CaseState = new() { Id = _caseState.Value };
+        }
+
         var _GetCartableSpec = new GetCartableSpec(cartableDTO: request.cartableDTO, cartableProperty: request.cartableProperty);
         var Records = await _WorkItemRepository.ListAsync(_GetCartableSpec, cancellationToken);
+
+        var _totalItems = await _WorkItemRepository.CountAsync(_GetCartableSpec, cancellationToken);
+
         var Inbox = Records
             .Select(x =>
             new CartableDTO()
             {
                 PageSize = request.cartableDTO.PageSize,
                 PageNumber = request.cartableDTO.PageNumber,
-                TotalItems = request.cartableDTO.TotalItems,
+                TotalItems = _totalItems,
                 Process = _iCopyClass
                 .GetSimpleClass(
                     _processRepository
@@ -37,10 +66,12 @@ public class GetCartableHandler(
                 WorkItemState = _iCopyClass.GetSimpleClass(x.WorkItemState),
                 Task = _iCopyClass.GetSimpleClass(
                     _taskRepository.FirstOrDefaultAsync(new GetEntitySpec<F_Task>(x.TaskId)).GetAwaiter().GetResult()),
-                //ValidUserActions =
-                //(_ISender.Send(new GetFlowsByTaskQuery(x.TaskId)).GetAwaiter().GetResult().Value)
-                //.SelectMany(a => a.Flow.FlowConditions,
-                //(a, b) => _iCopyClass.GetSimpleClass(b.Condition)),
+                ValidUserActions =
+                (_ISender.Send(new GetFlowsByTaskQuery(x.TaskId)).GetAwaiter().GetResult().Value)
+                .SelectMany(a => a.Flow.FlowConditions,
+                (a, b) => _iCopyClass.GetSimpleClass(
+                    _conditionRepository.FirstOrDefaultAsync(new GetEntitySpec<F_Condition>(b.SecondId)).GetAwaiter().GetResult())),
+
                 OccurredUserActions = x.WorkItemConditions.Select(x => _iCopyClass.GetSimpleClass(
                     _conditionRepository.FirstOrDefaultAsync(new GetEntitySpec<F_Condition>(x.SecondId)).GetAwaiter().GetResult())),
                 Summary = string.Empty
---
+++ src/Cheetah.Application.Business/Process/Process/Get/GetProcessHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Cheetah.Application.Business.ProcessScenario.Get;
+
+public class GetProcessHandler(
+    IReadRepository<D_Process> _processRepository) : IQueryHandler<GetProcessQuery, Result<IEnumerable<D_Process>>>
+{
+    public async Task<Result<IEnumerable<D_Process>>> Handle(GetProcessQuery request, CancellationToken cancellationToken)
+    {
+        var _processSpec = new GetProcessSpec(request.processIds);
+        var _processes = await _processRepository.ListAsync(_processSpec, cancellationToken);
+
+        return _processes;
+    }
+}
