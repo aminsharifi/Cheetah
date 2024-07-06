6dbe377dd3d7902cac9bc43e54830616d86eb19f
AminSharifi <m.amin.sharifi@gmail.com>
2024-07-06 12:28:01 +0330

    Feat: Add ListWorkItemsHistory

---
+++ src/Cheetah.Presentation.Services.WebAPI/Controllers/WorkflowEngine/Queries/ListWorkItemsHistory.ListWorkItemsHistoryResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Cheetah.Presentation.Services.WebAPI.Controllers.WorkflowEngine.Queries;
+
+public class ListWorkItemsHistoryResponse
+{
+    public BaseClassWithDateDTO? Case { get; set; }
+    public BaseClassWithNameDTO? Task { get; set; }
+    public BaseClassWithNameDTO? Form { get; set; }
+    public List<BaseClassWithNameDTO>? Performer { get; set; }
+    public BaseClassWithNameDTO? User { get; set; }
+    public BaseClassWithNameAndDateDTO? WorkItem { get; set; }
+    public BaseClassWithNameDTO? WorkItemState { get; set; }
+    public List<BaseClassWithNameDTO>? OccurredUserActions { get; set; }
+    public BaseClassWithNameDTO OutputState { get; set; } = default!;
+}
