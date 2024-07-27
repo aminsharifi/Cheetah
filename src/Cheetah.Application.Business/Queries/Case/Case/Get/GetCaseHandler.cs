71de97bed3a0c9f7f21a85f06c9ae863286d6e33
AminSharifi <m.amin.sharifi@gmail.com>
2024-07-27 15:05:56 +0330

    Initial Unit-Test for application (Use Case) Layer

--- src/Cheetah.Application.Business/Queries/Case/Case/Get/GetCaseHandler.cs
+++ src/Cheetah.Application.Business/Queries/Case/Case/Get/GetCaseHandler.cs
@@ -6,39 +6,39 @@ using Cheetah.Domain.Entities.Facts;
 namespace Cheetah.Application.Business.Queries.Case.Case.Get;
 
 public class GetCaseHandler(
-    IReadRepository<D_User> _userRepository,
-    IReadRepository<D_Process> _processRepository,
-    IReadRepository<F_Condition> _conditionRepository,
-    ISender _ISender, IMapper _IMapper) : IQueryHandler<GetCaseQuery, Result<F_Case>>
+    IReadRepository<D_User> userRepository,
+    IReadRepository<D_Process> processRepository,
+    IReadRepository<F_Condition> conditionRepository,
+    ISender iSender, IMapper iMapper) : IQueryHandler<GetCaseQuery, Result<F_Case>>
 {
     public async Task<Result<F_Case>> Handle(GetCaseQuery request, CancellationToken cancellationToken)
     {
-        F_Case _case = await GetCase.Handle(request: request, _userRepository: _userRepository,
-            _processRepository: _processRepository, _conditionRepository: _conditionRepository,
+        F_Case _case = await GetCase.Handle(Request: request, userRepository: userRepository,
+            processRepository: processRepository, conditionRepository: conditionRepository,
             cancellationToken: cancellationToken);
 
         _case = await GetCaseConditions
-         .Apply(f_case: _case, caseConditions: request.CaseConditions,
-         conditionRepository: _conditionRepository, iMapper: _IMapper,
-         cancellationToken: cancellationToken);
+            .Apply(f_case: _case, caseConditions: request.CaseConditions,
+            conditionRepository: conditionRepository, iMapper: iMapper,
+            cancellationToken: cancellationToken);
 
 
         var _workItemUserSpec = new GetIdEntitySpec<D_User>(request.WorkItemUser);
-        var _workItemUserId = await _userRepository.FirstOrDefaultAsync(_workItemUserSpec, cancellationToken);
+        var _workItemUserId = await userRepository.FirstOrDefaultAsync(_workItemUserSpec, cancellationToken);
 
         F_WorkItem _workItem = new F_WorkItem()
             .SetUserId(_workItemUserId);
 
         _workItem = await GetWorkItem
-            .Apply(iSender: _ISender,
+            .Apply(iSender: iSender,
             WorkItemUser: request.WorkItemUser, WorkItemBase: request.WorkItemBase,
-            workItemConditions: request.WorkItemConditions, _userRepository: _userRepository,
+            workItemConditions: request.WorkItemConditions, _userRepository: userRepository,
             workItem: _workItem, cancellationToken: cancellationToken);
 
         _workItem.SetCase(_case);
 
-        _case.WorkItems.Add(_workItem);
+        _case?.WorkItems?.Add(_workItem);
 
-        return _case;
+        return _case!;
     }
 }
---
+++ test/Cheetah.UnitTest/Application/Queries/Case/Case/Get/GetCaseHandlerHandle.cs
@@ -0,0 +1,58 @@
+﻿using Ardalis.SharedKernel;
+using Cheetah.Application.Business.Queries.Case.Case.Get;
+using Cheetah.Domain.Aggregates.ConditionAggregate.DTOs;
+using Cheetah.Domain.Common.DTOs;
+using Cheetah.Domain.Entities.Dimentions;
+using Cheetah.Domain.Entities.Facts;
+using MapsterMapper;
+using MediatR;
+using NSubstitute;
+
+namespace Cheetah.UnitTest.Application.Queries.Case.Case.Get;
+
+public class GetCaseHandlerHandle
+{
+    [Fact]
+    public async Task GetRelatedCase()
+    {
+        #region Arrange
+        IReadRepository<D_User> _userRepository = Substitute.For<IReadRepository<D_User>>();
+        IReadRepository<D_Process> _processRepository = Substitute.For<IReadRepository<D_Process>>();
+        IReadRepository<F_Condition> _conditionRepository = Substitute.For<IReadRepository<F_Condition>>();
+        ISender _iSender = Substitute.For<ISender>();
+        IMapper _iMapper = Substitute.For<IMapper>();
+
+        #region GetCaseQuery
+        SimpleClassDTO _case = new SimpleClassDTO();
+        SimpleClassDTO _creator = new SimpleClassDTO();
+        SimpleClassDTO _requestor = new SimpleClassDTO();
+        SimpleClassDTO _process = new SimpleClassDTO();
+        List<ConditionDTO> _caseConditions = new List<ConditionDTO>();
+        SimpleClassDTO _workItemUser = new SimpleClassDTO();
+        List<ConditionDTO> _workItemConditions = new List<ConditionDTO>();
+        SimpleClassDTO _workItemBase = new SimpleClassDTO();
+
+        GetCaseQuery _getCaseQuery = new GetCaseQuery(
+            Case: _case, Creator: _creator, Requestor: _requestor,
+            Process: _process, CaseConditions: _caseConditions,
+            WorkItemUser: _workItemUser, WorkItemConditions: _workItemConditions,
+            WorkItemBase: _workItemBase);
+        #endregion
+        #endregion
+
+        #region Act
+
+        GetCaseHandler _getCaseHandler = new GetCaseHandler(userRepository: _userRepository,
+            processRepository: _processRepository, conditionRepository: _conditionRepository,
+            iSender: _iSender, iMapper: _iMapper);
+
+        var _handle = await _getCaseHandler.Handle(_getCaseQuery, Arg.Any<CancellationToken>());
+        #endregion
+
+        #region Assert
+
+        Assert.True(_handle.IsSuccess);
+
+        #endregion
+    }
+}
