473d15f657f561b9d122e7efb228783438f1f0a4
MohammadAmin Sharifi <m.amin.sharifi@gmail.com>
2024-10-30 16:39:11 +0330

    feat: add LDAP service

---
+++ sample/src/Presentation/Services/WebAPI/Controllers/EntityManager/Commands/UpdateUsers.GetAllByNameValidator.cs
@@ -0,0 +1,7 @@
+﻿using Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Commands;
+
+namespace Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Queries;
+
+public class UpdateUsersValidator : Validator<UpdateUsers>
+{
+}
---
+++ sample/src/Presentation/Services/WebAPI/Controllers/EntityManager/Commands/UpdateUsers.cs
@@ -0,0 +1,56 @@
+﻿using Ardalis.SharedKernel;
+using Cheetah.Core.Aggregates.UserAggregate.Dimentions;
+using Cheetah.Core.Common.DTOs;
+using Cheetah.Core.Common.Specifications;
+using Cheetah.Core.Interfaces;
+using Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Queries;
+
+namespace Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Commands;
+
+public class UpdateUsers(ILogger<UpdateUsers> logger, ITableCRUD simpleClassRepository, IRepository<D_User> userRepository)
+    : Endpoint<UpdateUsersRequest, UpdateUsersResponse>
+{
+    public override void Configure()
+    {
+        Post(UpdateUsersRequest.Route);
+        AllowAnonymous();
+    }
+    public override async Task HandleAsync(UpdateUsersRequest request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("started " + nameof(UpdateUsers) + " {@" + nameof(UpdateUsers) + "}", request);
+
+        #region Input
+
+        UpdateUsersResponse output_Request = new();
+
+        #endregion
+
+        foreach (var _user in request.UserList)
+        {
+            var _getUser = await userRepository.FirstOrDefaultAsync(new GetEntitySpec<D_User>(new SimpleClassDTO() { Name = _user }, EnableTrack: true));
+
+            if (_getUser is null)
+            {
+                D_User user = (D_User)new D_User().SetName(_user);
+                await userRepository.AddAsync(user);
+            }
+            else
+            {
+                _getUser.SetName(_user);
+                await userRepository.UpdateAsync(_getUser!);
+            }
+        }
+
+        #region Output
+
+        output_Request.OutputState = new BaseClassWithNameDTO() { Id = 0 };
+
+        #endregion
+
+        logger.LogInformation("Ended " + nameof(UpdateUsers) + " {@" + nameof(UpdateUsers) + "}", output_Request);
+
+        Response = output_Request.Adapt<UpdateUsersResponse>();
+
+        return;
+    }
+}
