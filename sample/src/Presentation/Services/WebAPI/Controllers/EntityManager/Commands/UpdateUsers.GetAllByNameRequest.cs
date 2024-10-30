e2059c601e02c2968c6533fc74611d9ee83f1650
MohammadAmin Sharifi <m.amin.sharifi@gmail.com>
2024-10-30 16:39:11 +0330

    feat: add LDAP service

---
+++ sample/src/Presentation/Services/WebAPI/Controllers/EntityManager/Commands/UpdateUsers.GetAllByNameRequest.cs
@@ -0,0 +1,10 @@
+﻿using Cheetah.Core.Aggregates.UserAggregate.Dimentions;
+using Novell.Directory.Ldap;
+
+namespace Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Queries;
+
+public class UpdateUsersRequest
+{
+    public const string Route = "/EntityManager/Commands/UpdateUsers";
+    public List<string> UserList { get; set; }
+}
