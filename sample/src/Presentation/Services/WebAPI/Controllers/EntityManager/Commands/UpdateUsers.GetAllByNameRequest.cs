752c7037cc55879f6b7e682e8268e00aa82c4ef7
MohammadAmin Sharifi <m.amin.sharifi@gmail.com>
2024-11-01 11:41:12 +0330

    feat; add LDAP user loging

--- sample/src/Presentation/Services/WebAPI/Controllers/EntityManager/Commands/UpdateUsers.GetAllByNameRequest.cs
+++ sample/src/Presentation/Services/WebAPI/Controllers/EntityManager/Commands/UpdateUsers.GetAllByNameRequest.cs
@@ -1,10 +1,32 @@
-﻿using Cheetah.Core.Aggregates.UserAggregate.Dimentions;
-using Novell.Directory.Ldap;
-
-namespace Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Queries;
+﻿namespace Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Queries;
 
 public class UpdateUsersRequest
 {
     public const string Route = "/EntityManager/Commands/UpdateUsers";
-    public List<string> UserList { get; set; }
+    public List<DUser> UserList { get; set; }
+}
+public class DUser
+{
+    public long Id { get; set; } // bigint
+    public long? SortIndex { get; set; } // bigint
+    public string Name { get; set; } // nvarchar(512)
+    public string DisplayName { get; set; } // nvarchar(512)
+    public string Description { get; set; } // nvarchar(512)
+    public DateTimeOffset? Created { get; set; } // datetimeoffset(7)
+    public DateTimeOffset? LastModified { get; set; } // datetimeoffset(7)
+    public Guid? GuidRecord { get; set; } // uniqueidentifier
+    public bool EnableRecord { get; set; } // bit
+    public long? ERPCode { get; set; } // bigint
+    public string LastModifiedBy { get; set; } // nvarchar(512)
+    public string CreatedBy { get; set; } // nvarchar(512)
+    public string Domain { get; set; } // nvarchar(25)
+    public bool? EnabledForAssignation { get; set; } // bit
+    public bool? DelegateEnabled { get; set; } // bit
+    public bool? CreatedCasesSkipAssigRules { get; set; } // bit
+    public string IDPersonel { get; set; } // nvarchar(10)
+    public string LDAPDescription { get; set; } // nvarchar(20)
+    public long? DelegateId { get; set; } // bigint
+    public long? UserInformationId { get; set; } // bigint
+    public long? ParentId { get; set; } // bigint
+    public long? ManagerId { get; set; } // bigint
 }
--- sample/src/Presentation/Services/WebAPI/Controllers/EntityManager/Commands/UpdateUsers.cs
+++ sample/src/Presentation/Services/WebAPI/Controllers/EntityManager/Commands/UpdateUsers.cs
@@ -3,11 +3,16 @@ using Cheetah.Core.Aggregates.UserAggregate.Dimentions;
 using Cheetah.Core.Common.DTOs;
 using Cheetah.Core.Common.Specifications;
 using Cheetah.Core.Interfaces;
+using Cheetah.Sample.Infrastructure.Identity;
 using Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Queries;
+using Microsoft.AspNetCore.Identity;
 
 namespace Cheetah.Sample.Presentation.Services.WebAPI.Controllers.EntityManager.Commands;
 
-public class UpdateUsers(ILogger<UpdateUsers> logger, ITableCRUD simpleClassRepository, IRepository<D_User> userRepository)
+public class UpdateUsers(ILogger<UpdateUsers> logger,
+    ITableCRUD simpleClassRepository,
+    IRepository<D_User> userRepository,
+    UserManager<ApplicationUser> userManager)
     : Endpoint<UpdateUsersRequest, UpdateUsersResponse>
 {
     public override void Configure()
@@ -27,16 +32,32 @@ public class UpdateUsers(ILogger<UpdateUsers> logger, ITableCRUD simpleClassRepo
 
         foreach (var _user in request.UserList)
         {
-            var _getUser = await userRepository.FirstOrDefaultAsync(new GetEntitySpec<D_User>(new SimpleClassDTO() { Name = _user }, EnableTrack: true));
+            var _getUser = await userRepository
+                .FirstOrDefaultAsync(new GetEntitySpec<D_User>(new SimpleClassDTO() { Name = _user.Name! }, EnableTrack: true));
 
             if (_getUser is null)
             {
-                D_User user = (D_User)new D_User().SetName(_user);
+                D_User user = (D_User)new D_User()
+                    .SetName(_user.Name)
+                    .SetDisplayName(_user.DisplayName);
+
                 await userRepository.AddAsync(user);
+
+                ApplicationUser _applicationUser = new()
+                {
+                    UserName = _user.Name,
+                    Email = _user.Name + "@behparvar.com",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(_applicationUser);
             }
             else
             {
-                _getUser.SetName(_user);
+                _getUser
+                    .SetName(_user.Name)
+                    .SetDisplayName(_user.DisplayName);
+
                 await userRepository.UpdateAsync(_getUser!);
             }
         }
